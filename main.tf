1772214a638dc852343f3c5d8910d54e4bc6f7c9
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-12-12 13:42:54 +0530

    code formatting

--- main.tf
+++ main.tf
@@ -1,23 +1,21 @@
 module "parameter_store_task" {
-
-  dead_letter_arn = "${var.dead_letter_arn}"
-  handler         = "function.handler"
-  kms_key_arn     = "${var.kms_key_arn}"
+  dead_letter_arn = var.dead_letter_arn
+  handler         = function.handler
+  kms_key_arn     = var.kms_key_arn
   l3_object_key   = "quinovas/parameter-store-task/parameter-store-task-0.1.0.zip"
   name            = "${var.name_prefix}parameter-store-task"
 
   policy_arns = [
-    "${aws_iam_policy.parameter_store_task.arn}",
+    aws_iam_policy.parameter_store_task.arn,
   ]
 
-  policy_arns_count = 1
-  runtime           = "python3.7"
-  source            = "QuiNovas/lambdalambdalambda/aws"
-  timeout           = 300
-  version           = "0.2.0"
+  runtime = "python3.7"
+  source  = "QuiNovas/lambdalambdalambda/aws"
+  timeout = 300
+  version = "3.0.1"
 }
 
 resource "aws_iam_policy" "parameter_store_task" {
   name   = "${var.name_prefix}parameter-store-task"
-  policy = "${data.aws_iam_policy_document.parameter_store_task.json}"
+  policy = data.aws_iam_policy_document.parameter_store_task.json
 }
